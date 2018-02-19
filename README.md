b5cecda583901ab0b0baa2b972260a4e2241521e
Marc CARRE <carre.marc@gmail.com>
2018-02-19 13:45:50 +0000

    MVP/POC of imagediff.

---
+++ README.md
@@ -0,0 +1,39 @@
+# imagediff
+
+## What?
+
+Back-references two Docker images to their source code repository and prints the Git changelog between these two images, provided they are annotated with [`label-schema` labels](https://microbadger.com/labels).
+
+## Why?
+
+Unless properly labeled with relevant metadata, Docker containers are complete black boxes.
+One can hardly say which version of the code is packaged inside a container, and even less tie a container back to the source code repository...
+... unless you label it such information during CI.
+
+## How?
+
+- Add [`label-schema` labels](https://microbadger.com/labels). The two following labels are required as a bare minimum:
+
+  ```
+    LABEL org.label-schema.vcs-ref=$VCS_REF \
+          org.label-schema.vcs-url="e.g. https://github.com/microscaling/microscaling"
+  ```
+
+- Build Docker images
+- Run `imagediff` against these, which will:
+
+    - Pull the Docker images if required.
+    - Extract their labels.
+    - Extract the VCS' URL and commit hash from the labels.
+    - Clone the repository (in-memory).
+    - Perform a post-order traversal of the Git history, from the most recent change, to the oldest one, and prints that.
+
+## Example
+
+```bash
+$ go run main.go microscaling/microscaling:0.9.0 microscaling/microscaling:0.9.1
+45b22cb Merge pull request #40 from microscaling/k8s-labels
+91740fb Bump version
+309eece Use a separate KubeLabelConfig type for getting labels when using Kubernetes
+4756fd6 Get image from k8s deployment object so labels can be retrieved from the MicroBadger API. Move creating the k8s clientset to utils.
+```
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/term"
+	"github.com/src-d/go-git/storage/memory"
+
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		panic("Please provide two Docker image tags to compare")
+	}
+	x := os.Args[1]
+	y := os.Args[2]
+	docker, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	pull(docker, x)
+	pull(docker, y)
+	xLabels := imageLabels(docker, x)
+	yLabels := imageLabels(docker, y)
+	xVcsURL, xVcsRef := vcsURLAndRef(xLabels)
+	yVcsURL, yVcsRef := vcsURLAndRef(yLabels)
+	validate(x, y, xVcsURL, yVcsURL, xVcsRef, yVcsRef)
+	r := gitClone(xVcsURL)
+	xCommit := commit(r, xVcsRef)
+	yCommit := commit(r, yVcsRef)
+	printChangeLog(xCommit, yCommit)
+}
+
+func pull(docker *client.Client, imageName string) {
+	resp, err := docker.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Close()
+	fd, isTerminal := term.GetFdInfo(ioutil.Discard)
+	if err := jsonmessage.DisplayJSONMessagesStream(resp, ioutil.Discard, fd, isTerminal, nil); err != nil {
+		panic(err)
+	}
+}
+
+func imageLabels(docker *client.Client, imageName string) map[string]string {
+	args := filters.NewArgs()
+	args.Add("reference", imageName)
+	images, err := docker.ImageList(context.Background(), types.ImageListOptions{
+		Filters: args,
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, image := range images {
+		return image.Labels
+	}
+	panic("Image not found: " + imageName)
+}
+
+func vcsURLAndRef(labels map[string]string) (string, string) {
+	vcsURL := ""
+	vcsRef := ""
+	for label, value := range labels {
+		if strings.HasPrefix(label, "org.label-schema") {
+			switch label {
+			case "org.label-schema.vcs-url":
+				vcsURL = value
+			case "org.label-schema.vcs-ref":
+				vcsRef = value
+			}
+		}
+	}
+	return vcsURL, vcsRef
+}
+
+func validate(x, y, xVcsURL, yVcsURL, xVcsRef, yVcsRef string) {
+	if xVcsURL == "" {
+		panic("No repository for " + x)
+	}
+	if yVcsURL == "" {
+		panic("No repository for " + y)
+	}
+	if xVcsRef == "" {
+		panic("No commit hash for " + x)
+	}
+	if yVcsRef == "" {
+		panic("No commit hash for " + y)
+	}
+	if xVcsURL != yVcsURL {
+		panic("Source code in different repositories")
+	}
+}
+
+func gitClone(vcsURL string) *git.Repository {
+	storage := memory.NewStorage()
+	r, err := git.Clone(storage, nil, &git.CloneOptions{
+		URL: vcsURL,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+var errFound = errors.New("<Found>")
+
+// Workaround to resolve a short hash to a full commit object.
+// Once the following PR is merged, we should be able to do this in a more elegant way.
+// See also: https://github.com/src-d/go-git/pull/706
+func commit(r *git.Repository, shortHash string) *object.Commit {
+	commitsIter, err := r.Log(&git.LogOptions{From: plumbing.NewHash(shortHash)})
+	if err != nil {
+		panic(err)
+	}
+	var commit *object.Commit
+	err = commitsIter.ForEach(func(c *object.Commit) error {
+		if strings.HasPrefix(c.Hash.String(), shortHash) {
+			commit = c
+			return errFound
+		}
+		return nil
+	})
+	if err == nil || err != errFound {
+		panic(fmt.Sprintf("Commit with short hash %s could not be found: %s", shortHash, err))
+	}
+	return commit
+}
+
+func printChangeLog(xCommit, yCommit *object.Commit) {
+	err := object.NewCommitPostorderIter(yCommit, nil).ForEach(func(c *object.Commit) error {
+		if c.Hash == xCommit.Hash {
+			return errFound
+		}
+		fmt.Printf("%s %s\n", c.Hash.String()[:7], strings.Split(c.Message, "\n")[0])
+		return nil
+	})
+	if err == nil || err != errFound {
+		panic(fmt.Sprintf("Commit with hash %s could not be found: %s", xCommit.Hash, err))
+	}
+}
